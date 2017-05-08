c3212d5747bf32d6a6bc3d0d2c080ac4f0e343bc
Anthony Ponce <Anthony Ponce>
2017-05-08 16:17:58 -0700

    uploading files

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+</body>
+<script>
+$.ajax({
+      
+      method: curl -X GET \
+ -H 'X-Access-Token: 2fb7e08a9f8fbce9' \
+ 'https://api.eatstreet.com/publicapi/v1/restaurant/search?method=both&street-address=316+W.+Washington+Ave.+Madison,+WI'
+    }).done(function(response) {
+      console.log(response);
+  }
+ </script>
+</html>
