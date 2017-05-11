e0a62e4f2d9ee7973a68f5a4e03a85635f2147e6
Anthony Ponce <Anthony Ponce>
2017-05-11 11:17:48 -0700

    adding login sign up pop ups and ajax call to taco api

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,24 +8,34 @@
 // addb.illHaveOnes().loadSet(function(query) { });
 // addb.illHaveOnes().take(10).loadSet(function(query) { });
 
-//Eatstreet API
+// Eatstreet API
 
-        var es = document.createElement('script'); es.type = 'text/javascript'; es.async = true;
-        es.src = ('https:' == document.location.protocol ? 'https://' : 'http://developers.') + 'eatstreet.com/api-js-sdk/js/sdk-remote.js';
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(es, s);
+//         var es = document.createElement('script'); es.type = 'text/javascript'; es.async = true;
+//         es.src = ('https:' == document.location.protocol ? 'https://' : 'http://developers.') + 'eatstreet.com/api-js-sdk/js/sdk-remote.js';
+//         var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(es, s);
   
 
-window.onload = function(){
-				ESApi.init('2fb7e08a9f8fbce9');
+// window.onload = function(){
+// 				ESApi.init('2fb7e08a9f8fbce9');
 
-								var signin = {
-				    'email': 'newuser@eatstreet.com',
-				    'password': 'password'
-				};
-				ESApi.signinUser(signin, function(user) {
-				    console.log(user);
-				});
+// 								var signin = {
+// 				    'email': 'newuser@eatstreet.com',
+// 				    'password': 'password'
+// 				};
+// 				ESApi.signinUser(signin, function(user) {
+// 				    console.log(user);
+// 				});
 
-}
+// }
 
+$.ajax({
+      url: "http://taco-randomizer.herokuapp.com/random/?full-taco=true",
+      method: "GET"
+    }).done(function(response) {
+      console.log(response.name); 
+      console.log(response.recipe); 
+      console.log(response.condiment.name);
+      console.log(response.condidemnt.recipe)
+      console.log(response.base_layer)
 
+})
