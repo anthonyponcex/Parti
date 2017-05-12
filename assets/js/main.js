c51e832d753ad4b0e61713a99534726ce057b77d
Anthony Ponce <Anthony Ponce>
2017-05-12 16:40:53 -0700

    login

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,8 +29,7 @@ function redirectDashboard() {
   var token = result.credential.accessToken;
   // The signed-in user info.
   user = result.user;
-  grabUser();
-  redirectDashboard();
+ 
 
   // ...
 }).catch(function(error) {
