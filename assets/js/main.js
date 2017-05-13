4664c4ad6ecd7500d65caab975376c8301ae7d0f
Anthony Ponce <Anthony Ponce>
2017-05-12 18:00:52 -0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,9 +22,31 @@
 });
 }
 
-$("#login-btn").on('click', function() {
-	signIn();
-})
+if(user) {
+	window.location.href = "http://anthonyponcex.github.com/dashboard";
+}
+
+
+//Information on user//
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
+
+//Signout//
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+    });
+  }
+
+// $("#login-btn").on('click', function() {
+// 	signIn();
+// })
 
 
 // function signOut () {
