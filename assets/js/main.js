be903ebf4198a731115f13b040ff364882cf5f55
Anthony Ponce <Anthony Ponce>
2017-05-13 10:09:31 -0700

    sos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,142 +1,142 @@
 //Firebase//
 // Initialize Firebase
 
- var provider = new firebase.auth.GoogleAuthProvider();  
+//  var provider = new firebase.auth.GoogleAuthProvider();  
  
 
-  function signIn() {
-  	firebase.auth().signInWithPopup(provider).then(function(result) {
-  // This gives you a Google Access Token. You can use it to access the Google API.
-  var token = result.credential.accessToken;
-  // The signed-in user info.
-   var user = result.user;
-  // ...
-}).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // The email of the user's account used.
-  var email = error.email;
-  // The firebase.auth.AuthCredential type that was used.
-  var credential = error.credential;
-  // ...
-});
-}
+//   function signIn() {
+//   	firebase.auth().signInWithPopup(provider).then(function(result) {
+//   // This gives you a Google Access Token. You can use it to access the Google API.
+//   var token = result.credential.accessToken;
+//   // The signed-in user info.
+//    var user = result.user;
+//   // ...
+// }).catch(function(error) {
+//   // Handle Errors here.
+//   var errorCode = error.code;
+//   var errorMessage = error.message;
+//   // The email of the user's account used.
+//   var email = error.email;
+//   // The firebase.auth.AuthCredential type that was used.
+//   var credential = error.credential;
+//   // ...
+// });
+// }
 
-var auth2; // The Sign-In object.
-var googleUser; // The current user.
+// var auth2; // The Sign-In object.
+// var googleUser; // The current user.
 
-/**
- * Calls startAuth after Sign in V2 finishes setting up.
- */
-var appStart = function() {
-  gapi.load('auth2', initSigninV2);
-};
+// /**
+//  * Calls startAuth after Sign in V2 finishes setting up.
+//  */
+// var appStart = function() {
+//   gapi.load('auth2', initSigninV2);
+// };
 
-/**
- * Initializes Signin v2 and sets up listeners.
- */
-var initSigninV2 = function() {
-  auth2 = gapi.auth2.init({
-      client_id: 'CLIENT_ID.apps.googleusercontent.com',
-      scope: 'profile'
-  });
+// /**
+//  * Initializes Signin v2 and sets up listeners.
+//  */
+// var initSigninV2 = function() {
+//   auth2 = gapi.auth2.init({
+//       client_id: 'CLIENT_ID.apps.googleusercontent.com',
+//       scope: 'profile'
+//   });
 
-  // Listen for sign-in state changes.
-  auth2.isSignedIn.listen(signinChanged);
+//   // Listen for sign-in state changes.
+//   auth2.isSignedIn.listen(signinChanged);
 
-  // Listen for changes to current user.
-  auth2.currentUser.listen(userChanged);
+//   // Listen for changes to current user.
+//   auth2.currentUser.listen(userChanged);
 
-  // Sign in the user if they are currently signed in.
-  if (auth2.isSignedIn.get() == true) {
-    auth2.signIn();
-  }
+//   // Sign in the user if they are currently signed in.
+//   if (auth2.isSignedIn.get() == true) {
+//     auth2.signIn();
+//   }
 
-  // Start with the current live values.
-  refreshValues();
-};
+//   // Start with the current live values.
+//   refreshValues();
+// };
 
-/**
- * Listener method for sign-out live value.
- *
- * @param {boolean} val the updated signed out state.
- */
-var signinChanged = function (val) {
-  console.log('Signin state changed to ', val);
-  document.getElementById('signed-in-cell').innerText = val;
-};
+// /**
+//  * Listener method for sign-out live value.
+//  *
+//  * @param {boolean} val the updated signed out state.
+//  */
+// var signinChanged = function (val) {
+//   console.log('Signin state changed to ', val);
+//   document.getElementById('signed-in-cell').innerText = val;
+// };
 
-/**
- * Listener method for when the user changes.
- *
- * @param {GoogleUser} user the updated user.
- */
-var userChanged = function (user) {
-  console.log('User now: ', user);
-  googleUser = user;
-  updateGoogleUser();
-  document.getElementById('curr-user-cell').innerText =
-    JSON.stringify(user, undefined, 2);
-};
+// /**
+//  * Listener method for when the user changes.
+//  *
+//  * @param {GoogleUser} user the updated user.
+//  */
+// var userChanged = function (user) {
+//   console.log('User now: ', user);
+//   googleUser = user;
+//   updateGoogleUser();
+//   document.getElementById('curr-user-cell').innerText =
+//     JSON.stringify(user, undefined, 2);
+// };
 
-/**
- * Updates the properties in the Google User table using the current user.
- */
-var updateGoogleUser = function () {
-  if (googleUser) {
-    document.getElementById('user-id').innerText = googleUser.getId();
-    document.getElementById('user-scopes').innerText =
-      googleUser.getGrantedScopes();
-    document.getElementById('auth-response').innerText =
-      JSON.stringify(googleUser.getAuthResponse(), undefined, 2);
-  } else {
-    document.getElementById('user-id').innerText = '--';
-    document.getElementById('user-scopes').innerText = '--';
-    document.getElementById('auth-response').innerText = '--';
-  }
-};
+// /**
+//  * Updates the properties in the Google User table using the current user.
+//  */
+// var updateGoogleUser = function () {
+//   if (googleUser) {
+//     document.getElementById('user-id').innerText = googleUser.getId();
+//     document.getElementById('user-scopes').innerText =
+//       googleUser.getGrantedScopes();
+//     document.getElementById('auth-response').innerText =
+//       JSON.stringify(googleUser.getAuthResponse(), undefined, 2);
+//   } else {
+//     document.getElementById('user-id').innerText = '--';
+//     document.getElementById('user-scopes').innerText = '--';
+//     document.getElementById('auth-response').innerText = '--';
+//   }
+// };
 
-/**
- * Retrieves the current user and signed in states from the GoogleAuth
- * object.
- */
-var refreshValues = function() {
-  if (auth2){
-    console.log('Refreshing values...');
+// /**
+//  * Retrieves the current user and signed in states from the GoogleAuth
+//  * object.
+//  */
+// var refreshValues = function() {
+//   if (auth2){
+//     console.log('Refreshing values...');
 
-    googleUser = auth2.currentUser.get();
+//     googleUser = auth2.currentUser.get();
 
-    document.getElementById('curr-user-cell').innerText =
-      JSON.stringify(googleUser, undefined, 2);
-    document.getElementById('signed-in-cell').innerText =
-      auth2.isSignedIn.get();
+//     document.getElementById('curr-user-cell').innerText =
+//       JSON.stringify(googleUser, undefined, 2);
+//     document.getElementById('signed-in-cell').innerText =
+//       auth2.isSignedIn.get();
 
-    updateGoogleUser();
-  }
-}
+//     updateGoogleUser();
+//   }
+// }
 
 
 
-//Information on user//
-function onSignIn(googleUser) {
-  var profile = googleUser.getBasicProfile();
-  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-  console.log('Name: ' + profile.getName());
-  console.log('Image URL: ' + profile.getImageUrl());
-  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present
-  signIn();
-  var user = result.user;
+// //Information on user//
+// function onSignIn(googleUser) {
+//   var profile = googleUser.getBasicProfile();
+//   console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+//   console.log('Name: ' + profile.getName());
+//   console.log('Image URL: ' + profile.getImageUrl());
+//   console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present
+//   signIn();
+//   var user = result.user;
   
-}
+// }
 
-//Signout//
-function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-      console.log('User signed out.');
-    });
-  }
+// //Signout//
+// function signOut() {
+//     var auth2 = gapi.auth2.getAuthInstance();
+//     auth2.signOut().then(function () {
+//       console.log('User signed out.');
+//     });
+//   }
 
 
 
@@ -189,6 +189,59 @@ $("#random-taco-div").empty();
  	var firstName = snapshot.val(response.name);
  	cosone.log (firstName)
  	})
+
+$("#drinks-info").empty();
+//DRINKS//
+
+$("#recipe-btn").click(function(){
+  	$("#drinks-info").empty();
+	$.ajax({
+    		url: "http://www.thecocktaildb.com/api/json/v1/1/random.php",
+    		method:"GET"
+    	}).done(function(response) {
+
+    		console.log(response);
+
+    	
+
+    	function drinkRefresh (){
+
+    	}
+    		for (var i = 0; i < response.drinks.length; i++){
+    			console.log(response.drinks[i].strDrink);
+    			var bigDiv = $("<div>");
+    			var textDiv = $("<div>")
+    			var imageDiv = $("<div>")
+
+    			// imageDiv.attr("id", "")
+
+    			$("#drinks-info").append(bigDiv.append(imageDiv.addClass("col m6 s12 align center")));
+    			$("#drinks-info").append(bigDiv.append(textDiv.addClass("col m4 s12 align center")));
+
+
+    			
+    			textDiv.append( "<p><strong>Drink Name: </strong>" + response.drinks[i].strDrink + "</p>");
+    			textDiv.append( "<p><strong>Ingredients: </strong>" + response.drinks[i].strIngredient1 + ", " + response.drinks[i].strIngredient2 + ", " + response.drinks[i].strIngredient3 + ", " + response.drinks[i].strIngredient4 + "</p>");
+    			textDiv.append( "<p><strong>Mixing Instructions: </strong>" + response.drinks[i].strInstructions + "</p>");
+    			textDiv.append('<button class="btn-floating disabled " id="submit-taco-btn"><i class="material-icons teal lighten-2">add</i></a>');
+
+
+
+    			var image = $("<img class='materialboxed'>");
+
+    			image.attr('src', response.drinks[i].strDrinkThumb);
+
+    			imageDiv.append(image.attr('src', response.drinks[i].strDrinkThumb));
+    		}
+
+    	});
+})
+
+ $(document).ready(function(){
+    $('.materialboxed').materialbox();
+  });
+     
+       
    
 
 //////////////////////////Youtube//////////////////////////
