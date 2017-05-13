09b686239e1c29a4f4980bdc601530dc6633c3e5
Anthony Ponce <Anthony Ponce>
2017-05-12 18:39:30 -0700

    login

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,21 +25,21 @@
 
 
 
-// //Information on user//
-// function onSignIn(googleUser) {
-//   var profile = googleUser.getBasicProfile();
-//   console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-//   console.log('Name: ' + profile.getName());
-//   console.log('Image URL: ' + profile.getImageUrl());
-//   console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present
-//   signIn();
-//   var user = result.user;
-//   firebase.auth().onAuthStateChanged(function(user) {
-//   if (user) {
-//     window.location.href = "/dashboard"
-//   }
-// });
-// }
+//Information on user//
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present
+  signIn();
+  var user = result.user;
+  firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    window.location.href = "/dashboard"
+  }
+});
+}
 
 //Signout//
 function signOut() {
