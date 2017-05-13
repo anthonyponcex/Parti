cf4bd871d5d2544b7488f93d01fbbbf6b75e21ba
Anthony Ponce <Anthony Ponce>
2017-05-12 18:11:16 -0700

    lgin

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,13 +2,14 @@
 // Initialize Firebase
 
  var provider = new firebase.auth.GoogleAuthProvider();  
+ var user;
 
   function signIn() {
   	firebase.auth().signInWithPopup(provider).then(function(result) {
   // This gives you a Google Access Token. You can use it to access the Google API.
   var token = result.credential.accessToken;
   // The signed-in user info.
-  var user = result.user;
+  user = result.user;
   // ...
 }).catch(function(error) {
   // Handle Errors here.
